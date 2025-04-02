b8ba9d2286e01919e55ca375465a08ee2dddf066
matheus0810 <matheusgabriel934@gmail.com>
2025-04-02 11:22:59 -0300

    Incluindo pasta build/web para GitHub Pages

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "832971f643ae88b99ed004cf8583932f
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "1b7382441d71b3793e1d89164b7b618d",
+"flutter_bootstrap.js": "b817275e532a93f6d79ea00afd696df4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
